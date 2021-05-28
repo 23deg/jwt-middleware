70171317fa9f147e0221f5711b4cc4e45c76d536
aldorado <anuar.suarez@23degrees.io>
2021-05-28 16:49:48 +0200

    no token returns Unauthorized not BadRequest

--- jwt.go
+++ jwt.go
@@ -61,7 +61,7 @@ func (j *JWT) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	headerToken := req.Header.Get(j.authHeader)
 
 	if len(headerToken) == 0 {
-		http.Error(res, "Request error", http.StatusBadRequest)
+		http.Error(res, "Request error", http.StatusUnauthorized)
 		return
 	}
 	
